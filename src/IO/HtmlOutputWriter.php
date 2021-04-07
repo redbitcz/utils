976b917649069c73d514762b36cfaf41bfe56422
Jakub Bouček <pan@jakubboucek.cz>
2021-04-07 12:34:48 +0200

    Code style

--- src/IO/HtmlOutputWriter.php
+++ src/IO/HtmlOutputWriter.php
@@ -6,7 +6,6 @@ namespace Redbitcz\Utils\IO;
 
 class HtmlOutputWriter implements IOutStream
 {
-
     public function write(string $string): void
     {
         echo nl2br(htmlspecialchars($string));
