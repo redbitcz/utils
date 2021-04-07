d372f70b81f74a07cffd7effe7973dd4042e0f2b
Martin Dostál <martin.dostal@redbit.cz>
2021-04-07 10:11:11 +0200

    HtmlOutputWriter

---
+++ src/IO/HtmlOutputWriter.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Redbitcz\Utils\IO;
+
+class HtmlOutputWriter implements IOutStream
+{
+
+    public function write(string $string): void
+    {
+        echo nl2br(htmlspecialchars($string));
+    }
+
+    public function error(string $string): void
+    {
+        echo nl2br(htmlspecialchars($string));
+    }
+}
