d937827cbcb1d94e943499101025a593bfe50772
Martin Dostál <martin.dostal@redbit.cz>
2021-06-02 13:30:29 +0200

    Add stepInStep to Progress

--- examples/log/progress.php
+++ examples/log/progress.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
+use Redbitcz\Utils\IO\OutputWriter;
 use Redbitcz\Utils\Log\Logger;
 use Redbitcz\Utils\Log\Progress;
-use Redbitcz\Utils\IO\OutputWriter;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
@@ -13,8 +13,30 @@ $outputLogger = new Logger($writer);
 $progress = new Progress($outputLogger, 5);
 
 $outputLogger->info('Start progress');
-for ($i = 0; $i < 5; $i++) {
-    usleep(random_int(100000, 1000000));
-    $progress->step('This is one step #' . $i);
+for ($i = 1; $i <= 5; $i++) {
+    $progress->step('Proces #' . $i);
+    usleep(random_int(10000, 100000));
+    $progress->stepInStep('Create a box');
+    usleep(random_int(10000, 100000));
+    $progress->stepInStep('Take the paper', null, 2);
+    usleep(random_int(10000, 100000));
+    $progress->stepInStep('Find the paper in the closet', null, 3);
+    usleep(random_int(10000, 100000));
+    $progress->stepInStep('Take the paper in your hand', null, 3);
+    usleep(random_int(10000, 100000));
+    $progress->stepInStep('Put the paper on the table', null, 3);
+    usleep(random_int(10000, 100000));
+    $progress->stepInStep('Fold the paper into a box', null, 2);
+    usleep(random_int(10000, 100000));
+    $progress->stepInStep('Stick the box with duct tape', null, 2);
+    usleep(random_int(10000, 100000));
+    $progress->stepInStep('Take the duct tape', null, 3);
+    usleep(random_int(10000, 100000));
+    $progress->stepInStep('Stick it on the joints of the box', null, 3);
+    usleep(random_int(10000, 100000));
+    $progress->stepInStep('Put the finished box on the table', null, 2);
+    usleep(random_int(10000, 100000));
+    $progress->stepInStep('Put anything in the box');
+    usleep(random_int(10000, 100000));
 }
 $outputLogger->info('End progress');
